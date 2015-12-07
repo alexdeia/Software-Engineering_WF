54c81fd1b0a83541c1ceff4002964ea297af401a
alexdeia <alexk.deia@gmail.com>
2015-12-08 00:11:21 +0300

    Функция добавления записей
    костыль

--- P_I_WF/admin.Designer.cs
+++ P_I_WF/admin.Designer.cs
@@ -32,6 +32,14 @@
             this.b_exit = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.b_save = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb_show = new System.Windows.Forms.TextBox();
+            this.tb_time = new System.Windows.Forms.TextBox();
+            this.tb_leading = new System.Windows.Forms.TextBox();
+            this.tb_links = new System.Windows.Forms.TextBox();
+            this.tb_duration = new System.Windows.Forms.TextBox();
+            this.tb_advert = new System.Windows.Forms.TextBox();
+            this.tb_comments = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -60,13 +68,13 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 63);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(847, 258);
+            this.dataGridView1.Size = new System.Drawing.Size(847, 179);
             this.dataGridView1.TabIndex = 3;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // b_save
             // 
-            this.b_save.Location = new System.Drawing.Point(748, 327);
+            this.b_save.Location = new System.Drawing.Point(748, 248);
             this.b_save.Name = "b_save";
             this.b_save.Size = new System.Drawing.Size(111, 46);
             this.b_save.TabIndex = 4;
@@ -74,11 +82,78 @@
             this.b_save.UseVisualStyleBackColor = true;
             this.b_save.Click += new System.EventHandler(this.b_save_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(773, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb_show
+            // 
+            this.tb_show.Location = new System.Drawing.Point(15, 306);
+            this.tb_show.Name = "tb_show";
+            this.tb_show.Size = new System.Drawing.Size(100, 20);
+            this.tb_show.TabIndex = 6;
+            // 
+            // tb_time
+            // 
+            this.tb_time.Location = new System.Drawing.Point(121, 306);
+            this.tb_time.Name = "tb_time";
+            this.tb_time.Size = new System.Drawing.Size(100, 20);
+            this.tb_time.TabIndex = 7;
+            // 
+            // tb_leading
+            // 
+            this.tb_leading.Location = new System.Drawing.Point(227, 306);
+            this.tb_leading.Name = "tb_leading";
+            this.tb_leading.Size = new System.Drawing.Size(100, 20);
+            this.tb_leading.TabIndex = 8;
+            // 
+            // tb_links
+            // 
+            this.tb_links.Location = new System.Drawing.Point(333, 306);
+            this.tb_links.Name = "tb_links";
+            this.tb_links.Size = new System.Drawing.Size(100, 20);
+            this.tb_links.TabIndex = 9;
+            // 
+            // tb_duration
+            // 
+            this.tb_duration.Location = new System.Drawing.Point(439, 306);
+            this.tb_duration.Name = "tb_duration";
+            this.tb_duration.Size = new System.Drawing.Size(100, 20);
+            this.tb_duration.TabIndex = 10;
+            // 
+            // tb_advert
+            // 
+            this.tb_advert.Location = new System.Drawing.Point(545, 306);
+            this.tb_advert.Name = "tb_advert";
+            this.tb_advert.Size = new System.Drawing.Size(100, 20);
+            this.tb_advert.TabIndex = 11;
+            // 
+            // tb_comments
+            // 
+            this.tb_comments.Location = new System.Drawing.Point(651, 306);
+            this.tb_comments.Name = "tb_comments";
+            this.tb_comments.Size = new System.Drawing.Size(100, 20);
+            this.tb_comments.TabIndex = 12;
+            // 
             // admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(871, 385);
+            this.ClientSize = new System.Drawing.Size(871, 568);
+            this.Controls.Add(this.tb_comments);
+            this.Controls.Add(this.tb_advert);
+            this.Controls.Add(this.tb_duration);
+            this.Controls.Add(this.tb_links);
+            this.Controls.Add(this.tb_leading);
+            this.Controls.Add(this.tb_time);
+            this.Controls.Add(this.tb_show);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.b_save);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.b_exit);
@@ -98,5 +173,13 @@
         private System.Windows.Forms.Button b_exit;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button b_save;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb_show;
+        private System.Windows.Forms.TextBox tb_time;
+        private System.Windows.Forms.TextBox tb_leading;
+        private System.Windows.Forms.TextBox tb_links;
+        private System.Windows.Forms.TextBox tb_duration;
+        private System.Windows.Forms.TextBox tb_advert;
+        private System.Windows.Forms.TextBox tb_comments;
     }
 }
--- P_I_WF/admin.cs
+++ P_I_WF/admin.cs
@@ -14,9 +14,12 @@ namespace P_I_WF
     public partial class admin : Form
     {
         public SQLiteConnection l_con = new SQLiteConnection(@"data source=D:\Program Files\Project\P_I_WF\users.db");
+
         public admin()
         {
             InitializeComponent();
+
+
         }
 
         private void b_exit_Click(object sender, EventArgs e)
@@ -35,25 +38,84 @@ namespace P_I_WF
             DataTable dt = new DataTable();
             dt.Load(dr);
             dataGridView1.DataSource = dt;
-            // dataGridView1.DataBind();
             l_con.Close();
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            l_con.Open();
-            SQLiteCommand cmd_save = l_con.CreateCommand();
-            cmd_save.CommandText = "UPDATE * FROM tvprog";
-            SQLiteDataReader drs = cmd_save.ExecuteReader(CommandBehavior.CloseConnection);
-            DataTable dts = new DataTable();
-            dts.Load(drs);
-            dataGridView1.DataSource = dts;
-            l_con.Close();
         }
 
         private void b_save_Click(object sender, EventArgs e)
         {
+            //l_con.Open();
 
+            //// DATABASE (Local): Formulate the SQL command.
+            //String strSqlCommand = "SELECT * FROM tvprog";
+            //System.Data.SQLite.SQLiteCommand oLocalCommand = new System.Data.SQLite.SQLiteCommand(strSqlCommand, l_con);
+
+            //// DATABASE (Local): Get the data records.
+            //SQLiteDataAdapter oLocalAdapter = new SQLiteDataAdapter(oLocalCommand);
+            //oLocalAdapter.AcceptChangesDuringUpdate = true;
+            //DataSet oLocalSet = new DataSet();
+            //oLocalAdapter.Fill(oLocalSet, "tvprog");
+
+            //// 
+            //SQLiteCommandBuilder oBuilder = new SQLiteCommandBuilder(oLocalAdapter);
+
+            //// Try to write to some changes.
+            //String strValue = oLocalSet.Tables[0].Rows[0][7].ToString();
+            //oLocalSet.Tables[0].Rows[0][7] = 8;
+            //strValue = oLocalSet.Tables[0].Rows[0][7].ToString();
+            //oLocalSet.AcceptChanges();
+            //oLocalAdapter.UpdateCommand = oBuilder.GetUpdateCommand();
+            //oLocalAdapter.Update(oLocalSet.Tables[0]);
+
+            //// Clean up.
+            //oLocalSet.Dispose();
+            //oLocalAdapter.Dispose();
+            //oLocalCommand.Dispose();
+            //oLocalCommand = null;
+
+            ////SQLiteDataAdapter da = new SQLiteDataAdapter("SELECT * FROM tvprog", l_con);
+            ////da.UpdateCommand = new SQLiteCommand("UPDATE * FROM tvprog", l_con);
+            ////SQLiteCommandBuilder cb = new SQLiteCommandBuilder(da);
+            ////da.UpdateCommand = cb.GetUpdateCommand();
+            //l_con.Close();
+
+
+            ////l_con.Open();
+            ////SQLiteCommand cmd_save = l_con.CreateCommand();
+            ////cmd_save.CommandText = "UPDATE * FROM tvprog";
+            ////SQLiteDataReader drs = cmd_save.ExecuteReader(CommandBehavior.CloseConnection);
+            ////DataTable dts = new DataTable();
+            ////dts.Load(drs);
+            ////dataGridView1.DataSource = dts;
+            ////l_con.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                l_con.Open();
+                SQLiteCommand cmd_add = l_con.CreateCommand();
+                cmd_add.CommandText = "INSERT INTO [tvprog] ([show],[time],[leading],[links],[duration],[advert],[comments]) VALUES ('" + tb_show.Text + "', '" + tb_time.Text + "', '" + tb_leading.Text + "', '" + tb_links.Text + "', '" + tb_duration.Text + "', '" + tb_advert.Text + "', '" + tb_comments.Text + "')";
+                if (cmd_add.ExecuteNonQuery() == 1)
+                    MessageBox.Show("Запись добавлена!");
+
+                SQLiteCommand cmd_view = l_con.CreateCommand();
+                cmd_view.CommandText = "SELECT * FROM tvprog";
+                SQLiteDataReader dr = cmd_view.ExecuteReader(CommandBehavior.CloseConnection);
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                dataGridView1.DataSource = dt;
+
+                l_con.Close();
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
